dd762d866f0500a812135062b57cc791f409bb12
Nicholas Cannon <nicholascannon1@gmail.com>
2021-11-28 17:15:18 +0800

    remove zshrc sync from install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "Installing env..."
 
-ln -s -f $PWD/.zshrc $HOME/.zshrc
+#ln -s -f $PWD/.zshrc $HOME/.zshrc
 ln -s -f $PWD/.vimrc $HOME/.vimrc
 ln -s -f $PWD/.coc.vim $HOME/.coc.vim
 ln -s -f $PWD/.tmux.conf $HOME/.tmux.conf
