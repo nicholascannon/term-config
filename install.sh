f8782c0fae231b80f564832a2ab248888db1437f
Nicholas Cannon <nicholascannon1@gmail.com>
2021-11-28 17:13:10 +0800

    install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+echo "Installing env..."
+
+ln -s -f $PWD/.zshrc $HOME/.zshrc
+ln -s -f $PWD/.vimrc $HOME/.vimrc
+ln -s -f $PWD/.coc.vim $HOME/.coc.vim
+ln -s -f $PWD/.tmux.conf $HOME/.tmux.conf
+ln -s -f $PWD/.prettierrc $HOME/.prettierrc
+
+echo "Done"
+
