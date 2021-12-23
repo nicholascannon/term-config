fa71fd1e315eef230e5436f4fa7d0e83daa13e8a
Nicholas Cannon <nicholascannon1@gmail.com>
2021-12-23 19:53:09 +0800

    move dotfiles

--- install-apps.sh
+++ install-apps.sh
@@ -2,10 +2,10 @@
 
 echo "Installing app configs..."
 
-ln -s -f $PWD/.vimrc $HOME/.vimrc
-ln -s -f $PWD/.coc.vim $HOME/.coc.vim
-ln -s -f $PWD/.tmux.conf $HOME/.tmux.conf
-ln -s -f $PWD/.prettierrc $HOME/.prettierrc
+ln -s -f $PWD/dotfiles/.vimrc $HOME/.vimrc
+ln -s -f $PWD/dotfiles/.coc.vim $HOME/.coc.vim
+ln -s -f $PWD/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s -f $PWD/dotfiles/.prettierrc $HOME/.prettierrc
 
 echo "Done"
 
--- install-shell.sh
+++ install-shell.sh
@@ -2,7 +2,7 @@
 
 echo "Installing zsh..."
 
-ln -s -f $PWD/.zshrc $HOME/.zshrc
+ln -s -f $PWD/dotfiles/.zshrc $HOME/.zshrc
 
 echo "Done"
 
