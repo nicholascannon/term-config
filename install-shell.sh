cc076c0359f04ccfa974f74d5082a58c976262b0
nicholascannon <nicholascannon1@gmail.com>
2022-09-07 16:49:44 +0800

    add vim plug to install script

--- install-shell.sh
+++ install-shell.sh
@@ -17,5 +17,9 @@ echo "Installing fnm..."
 
 brew install fnm
 
+echo "Installing vim-plug..."
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 echo "Done!"
 
